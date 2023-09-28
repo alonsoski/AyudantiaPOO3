e62ed9ba6e253530232256f133c2455385e11aa2
alonsoski <j.alonso01@ufromail.cl>
2023-09-28 14:58:20 -0300

    Creacion de las clases: -Libro. -Biblioteca.
    Creacion de los metodos "setters" y "getters" de cada clase.

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Biblioteca {
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Libro> libros;
+    private String nombreBiblio;
+    private String direccion;
+
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getNombreBiblio() {
+        return nombreBiblio;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setNombreBiblio(String nombreBiblio) {
+        this.nombreBiblio = nombreBiblio;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+    public void agregarUnLibro(Libro libro){
+        this.libros.add(libro);
+    }
 }
+
--- src/main/java/Libro.java
+++ src/main/java/Libro.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Libro {
+public class Libro {
+    private String nombre;
+    private String editorial;
+    private String autor;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
 }
